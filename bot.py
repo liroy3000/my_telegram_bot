c74c7b89a889ad1ae6bca2197609ee701db78e2c
Konstantin Kulkov <u6157@of61.kib.ru>
2019-07-19 17:19:18 +0300

    Enable enable infinite loop

--- bot.py
+++ bot.py
@@ -9,3 +9,14 @@ bot = telebot.TeleBot(config.token)
 @bot.message_handler(commands=['ip']):
 def send_ip(message):
 	bot.send_message(message.chat.id, func.show_ip())
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
