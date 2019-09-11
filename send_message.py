53ae65f3b0de36253c153d5d229656c87f51e5bc
Konstantin Kulkov <u6157@of61.kib.ru>
2019-09-11 11:27:25 +0300

    add search proxy server

--- send_message.py
+++ send_message.py
@@ -2,8 +2,11 @@
 import config
 import requests
 from sys import argv
+import func
 
+proxy_server = func.get_proxy_list('HTTPS')[0]
+proxy_server ='https://' + proxy_server['address'] + ':' + proxy_server['port']
 message = argv[1]
 url = 'https://api.telegram.org/bot' + config.token + '/sendMessage'
 params = {'chat_id': config.chat_id, 'text': message}
-requests.post(url, data=params, proxies={'https': config.proxy})
+requests.post(url, data=params, proxies={'https': proxy_server})
